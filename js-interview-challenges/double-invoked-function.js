ac14d010dd6bd383327f6ee38e06103432a810b9
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-07 15:31:31 -0800

    Create double-invoked-function.js

---
+++ js-interview-challenges/double-invoked-function.js
@@ -0,0 +1,17 @@
+function getTotal() {
+  var args = Array.prototype.slice.call(arguments);
+  
+  if (args.length === 2) {
+    return args[0] + args[1];
+  }
+  else if (args.length === 1) {
+    return function(num2) {
+      return args[0] + num2;
+    };
+  }
+}
+ 
+console.log(getTotal(10, 20));
+console.log(getTotal(5, 40));
+console.log(getTotal(3)(30));
+console.log(getTotal(8)(12));

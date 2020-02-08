9a8049180301e884a48f633f9e83de0ea9f6dee1
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-08 13:46:43 -0800

    Create big0-notation-O(n).js

---
+++ algos/big0-notation-O(n).js
@@ -0,0 +1,22 @@
+
+function logAll (array){
+  for ( var i = 0; i < array.length ; i++){
+    console.log([i])
+  }
+}
+
+function log (array){
+  console.log(array[0]);
+  console.log(array[1]);
+}
+
+//4 operations
+log([1,2,3,4]) 
+
+//6 operations
+log([1,2,3,4,5,6])
+
+//10 operations
+log([1,2,3,4,5,6,7,8,9,10])
+
+//lineral ... 0(n)

94f15d93f8c33c468d0f14ec5673f917275ececa
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-08 15:59:47 -0800

    Update two-objects.js

--- algos/two-objects.js
+++ algos/two-objects.js
@@ -1,3 +1,18 @@
+// const user1 = {
+//   name: 'Jordan',
+//   age: 28,
+// };
+ 
+// const user2 = {
+//   name: 'Jordan',
+//   age: 28,
+// };
+ 
+// console.log(user1 == user2); //false
+// console.log(user1 === user2); //false
+
+
+////////////////solution 
 const user1 = {
   name: 'Jordan',
   age: 28,
@@ -9,4 +24,4 @@ const user2 = {
 };
  
 console.log(user1 == user2);
-console.log(user1 === user2);
+console.log(JSON.stringify(user1) === JSON.stringify(user2));

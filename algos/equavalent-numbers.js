99e2a764b8eef9a576a6b5f385d91cc9cd37b0a8
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-08 18:46:24 -0800

    Create equavalent-numbers.js

---
+++ algos/equavalent-numbers.js
@@ -0,0 +1,5 @@
+console.log(900.9 === 300.3 * 3);
+console.log(300.3 * 3);
+console.log(Number((300.3 * 3).toFixed(2)));
+console.log(Number((300.3 * 3).toPrecision(12)));
+console.log(((300.3 * 10) * 3) / 10);
